934c337d65b9dcc35fe8f4f3ffff8c9813c44899
Fiddel <danf95@gmail.com>
2022-12-22 18:56:13 +0200

    react

--- venv/static/test.js
+++ venv/static/test.js
@@ -1,6 +1,9 @@
 console.log("true!")
 function submt(){
 	btm = document.getElementById("myform");
-	btm.submit();
+	//btm.submit();
+	f = new FormData(btm);
+	fetch("http://127.0.0.1:5000/ping", {body: f, method: "post"});
+	
 	btm.reset();
 }
