192c4b3b2743dd7b42f67d0578d9a7235e63d72b
Fiddel <danf95@gmail.com>
2022-12-29 03:02:47 +0200

    send_pair

--- venv/hello.py
+++ venv/hello.py
@@ -9,34 +9,37 @@ def hello_world():
 
 @app.route("/ping", methods=["GET"])
 def GET_ping():
+	print("t")
 	return render_template("student_reg.html")
 
 @app.route("/ping", methods=["POST"])
 def POST_ping():
 	data = request.form.to_dict()
 	print(data, type(data))
+	print("test")
 	
 	with open("test.txt", "r") as ts:
 		ts_data = json.load(ts)
 		print(ts_data, type(ts_data))
 	
-	ts_data = pair_student(ts_data, data)
+	pair, ts_data = pair_student(ts_data, data)
 	
 	with open("test.txt", "w") as ts:
 		print(ts_data)
 		json.dump(ts_data, ts)
-	return "got POST"
+	return pair
 
 def pair_student(std_list, new_std):
+	print("test")
 	for std in std_list:
 		if std["drop_cours"] == new_std["take_cours"]:
 			if std["take_cours"] == new_std["drop_cours"]:
 				print(std, new_std)
 				std_list.remove(std)
-				return std_list
+				return [std, new_std], std_list
 	
 	std_list.append(new_std)
-	return std_list
+	return [], std_list
 
 
 
