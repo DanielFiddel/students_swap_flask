0e4cc2729c3c3fb310dfdd9b74b1dfce60c6f4bd
Fiddel <danf95@gmail.com>
2023-01-12 17:23:00 +0200

    added archive

--- venv/hello.py
+++ venv/hello.py
@@ -1,6 +1,7 @@
 from flask import *
 from flask_cors import CORS
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -11,37 +12,77 @@ def hello_world():
 
 @app.route("/ping", methods=["GET"])
 def GET_ping():
-	print("t")
-	return render_template("student_reg.html")
+	with open("arc.txt", "r") as arc:
+		arc_data = json.load(arc)
+	return arc_data[::-1]
+	
+@app.route("/queue", methods=["GET"])
+def GET_queue():
+	with open("test.txt", "r") as qu:
+		qu_data = json.load(qu)
+	return qu_data[::-1]
+
+@app.route("/remove", methods=["POST"])
+def Post_remove():
+	data = request.form.to_dict()
+	#print(data)
+	
+	with open("test.txt", "r") as st:
+		st_list = json.load(st)
+	
+	if data in st_list:
+		st_list.remove(data)
+		
+		with open("test.txt", "w") as st:
+			#print(st_list)
+			json.dump(st_list, st)
+			return "removed"
+	
+	return "not in queue"
 
 @app.route("/ping", methods=["POST"])
 def POST_ping():
 	data = request.form.to_dict()
-	print(data, type(data))
-	print("test")
+	#print(data, type(data))
+	#print("test")
 	
 	with open("test.txt", "r") as ts:
 		ts_data = json.load(ts)
-		print(ts_data, type(ts_data))
+		#print(ts_data, type(ts_data))
 	
 	pair, ts_data = pair_student(ts_data, data)
 	
 	with open("test.txt", "w") as ts:
-		print(ts_data)
+		#print(ts_data)
 		json.dump(ts_data, ts)
 	return pair
 
 def pair_student(std_list, new_std):
-	print("test")
+	#print("test")
 	for std in std_list:
 		if std["drop_cours"] == new_std["take_cours"]:
 			if std["take_cours"] == new_std["drop_cours"]:
-				print(std, new_std)
+				#print(std, new_std)
+				archive([std, new_std])
 				std_list.remove(std)
 				return [std, new_std], std_list
 	
 	std_list.append(new_std)
 	return [], std_list
+
+def archive(data):
+	time = datetime.now().strftime("%d/%m/%y %H:%M:%S")
+	data.append(time)
+	with open("arc.txt", "r") as arc:
+		arc_data = json.load(arc)
+		
+	arc_data.append(data)
+	
+	with open("arc.txt", "w") as arc:
+		#print(arc_data)
+		json.dump(arc_data, arc)
+
+
 
 
 
---
+++ venv/tasks.md
@@ -0,0 +1,14 @@
+tasks
+===
+- [ ] loops of More than 2 students
+- [ ] Save more than one loop
+	- [ ] test loop of three
+- [ ] Ineterface for the secretary
+	- [ ] Loop graphics for the secretary
+- [ ] minimal graphics
+- [ ] Graphics for the students
+- [ ] Show various loop optioons to the student/secretary
+- [ ] Find maximum loop
+- [ ] Relrvant timings
+- [x] option to bailout for student
+- [x] show queue
